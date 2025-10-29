f36685c69ccb7c1cd91d82c6a22914e49352ecca
Sven Schoppe <sven@5worx.de>
2025-10-29 08:27:30 +0100

    Iniitial LazyVim config setup

---
+++ lua/config/options.lua
@@ -0,0 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Setzt Auto-Speichern beim Verlassen des Buffers oder wenn das Fenster den Fokus verliert
+vim.opt.autowriteall = true
